f82e6f90e49fd80f67bf719302f95f27eb060409
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2025-05-22 11:25:26 -0600

    primer comit jose

--- v4posme_printer_window_services/Program.cs
+++ v4posme_printer_window_services/Program.cs
@@ -2,6 +2,6 @@ using v4posme_printer_window_services;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
-
+//Comentario test
 var host = builder.Build();
 host.Run();
